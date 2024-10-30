ca39533b3bab83737b87ec9c1c4b5ca7cc3c0b10
ujin <hyeran0920@naver.com>
2024-10-31 03:44:31 +0900

    chat project

---
+++ server.js
@@ -0,0 +1,44 @@
+// Express 서버와 Socket.io를 설정하고, ViteExpress로 Vite 개발 서버와 Express 서버를 통합해 동작
+import { Server } from 'socket.io';
+import express from "express";
+import * as http from "http";
+import ViteExpress from "vite-express"
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {cors: {
+    origin: "https://example.com"
+  }});
+
+io.on('connection', (client) => {
+    console.log(client.handshake.query);
+    
+    const connectedClientUserName = client.handshake.query.username;
+    console.log(`${connectedClientUserName} 사용자가 들어왔습니다!`);
+
+    client.broadcast.emit('new message', {username : "관리자", message:`${connectedClientUserName} 님이 들어왔습니다!`})
+
+
+    client.on ('new message', (msg) =>{
+        console.log(`${connectedClientUserName} 사용자의 메세지 :  ${msg}`);
+        io.emit('new message', {username : msg.username, message: msg.message})
+
+    })    
+    
+    client.on ('disconnect', () =>{
+        console.log(`${connectedClientUserName} 사용자가 나갔습니다.`);
+        io.emit('new message', {username : "관리자", message:`${connectedClientUserName} 님이 나갔습니다.`})
+
+    })
+    
+});
+
+server.listen(3000, () => {
+    console.log('3000 서버에서 듣고있습니다.')
+});//서버 설정한걸 3000이 받는다
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+app.get("/api", (_, res) => res.send("Hello from API!"));
+
+ViteExpress.bind(app, server);
---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+import { useState, useEffect } from 'react'
+import { FaArrowUp } from "react-icons/fa"; // 화살표 아이콘 가져오기
+
+import './App.css'
+import {io} from "socket.io-client"
+
+function App() {
+  const [isConnected, setisConnected] = useState(false);
+
+  const [messages, setMessages] = useState([]);
+
+  const [username, setUsername] = useState('');
+  const [socket, setSocket] = useState(null);  
+  
+  const [userInput, setUserInput] = useState('');
+
+  function connectToChatServer(){
+      console.log('connectToChatServer');
+      const _socket = io('http://localhost:3000',{
+        autoConnect : false,
+        query:{
+          username : username,
+        }
+      });
+      _socket.connect(); 
+
+      setSocket(_socket);
+    }
+
+    function disconnectToChatServer(){
+      console.log('connectToChatServer');
+      socket?.disconnect(); //물음표는 소켓이 있는지 없는지 모르겠지만 일단 disconnect하는것
+    }
+
+    function onConnected(){
+      console.log('연결되었습니다')
+      setisConnected(true);
+    }
+    function onDisconnected(){
+      console.log('종료되었습니다')
+      setisConnected(false);
+    }
+
+    function sendMessageToChatServer(){
+      console.log(`메세지 전송 ${userInput}` )
+      socket?.emit("new message",{username: username, message : userInput},(response) =>{
+        console.log(response);
+      });
+      setUserInput('');
+    }
+
+    function onMessageRecived(msg){
+      console.log(msg)
+      setMessages(previous => [...previous, msg]);
+    }
+
+    useEffect(() => {
+      console.log('화면 스크롤' )
+
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        left: 0,
+        behavior: "smooth" 
+      });
+    }, [messages]);
+    
+
+    useEffect(() => {
+      console.log('useEffact called!');
+      socket?.on('connect', onConnected)
+      socket?.on('disconnect', onDisconnected)
+
+      socket?.on('new message', onMessageRecived)
+
+      return () => {
+        console.log('useEffect clean up function called!')
+        socket?.off('connect', onConnected)
+        socket?.off('disconnect', onDisconnected)
+        socket?.off('new message', onMessageRecived)
+      };
+    }, [socket]);
+
+    const messageList = messages.map((aMsg, index) =>
+      <li key={index}>
+        {aMsg.username} : {aMsg.message}
+      </li>
+    );
+
+
+  return (
+    <>
+      <div className="Navbar">
+        {isConnected ? (
+          // 접속 중일 때
+          <>
+            <h4 style={{ textAlign: "right" }}>
+              유저 : {username} 접속상태 : {isConnected ? "접속중" : "미접속"}&nbsp;&nbsp;<br/><br/>
+            <button  onClick={() => disconnectToChatServer()}>
+              접속종료
+            </button>
+            </h4>
+          </>
+        ) : (
+          // 미접속 중일 때
+          <div className="Card">
+            <h3>닉네임을 입력해주세요!</h3><br/>
+            <input 
+              value={username} 
+              onChange={e => setUsername(e.target.value)} 
+              placeholder="닉네임을 입력해주세요" 
+            />&nbsp;
+            <button onClick={() => connectToChatServer()}>
+              접속하기
+            </button>
+          </div>
+        )}
+      </div>
+
+      <ul className='ChatList'>
+        {messageList}
+      </ul>
+      
+        <div className="MessageInput">
+          <input value={userInput} onChange={e => setUserInput(e.target.value)}          
+          placeholder="채팅을 입력해주세요" />
+          <button onClick={() => sendMessageToChatServer()}>
+          <FaArrowUp /> {/* 위쪽 화살표 아이콘 */}
+          </button>
+        </div>
+
+        
+    </>
+  )
+}
+
+export default App
