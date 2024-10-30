1bb3eef0f9b90c0a6db454d56c0c6eda5ecb2e4f
ujin <hyeran0920@naver.com>
2024-10-31 04:18:24 +0900

    change cros

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {cors: {
-    origin: "https://example.com"
+    origin: "*"
   }});
 
 io.on('connection', (client) => {
